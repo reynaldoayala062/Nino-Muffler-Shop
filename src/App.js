32b1582f56a80132e9009217a1e973625814ec7e
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-01-26 18:40:37 -0600

    home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import logo from './images/1616025-middle.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="shake" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Nino Muffler and more!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
